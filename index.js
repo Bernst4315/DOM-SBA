7f6852f57d2ddbaaf8c88e537d6e3d418c30d42f
bernst4315 <bernst4315@gmail.com>
2024-12-18 15:46:36 -0700

    added a button

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const clicked = document.getElementById("btn");
 const treeBtn = document.getElementById("tree-btn")
 const main = document.querySelector("#main")
+const addTextBtn = document.getElementById("add-text")
 let toggle = true;
 let makeDiv = document.createElement("div");
 
@@ -23,6 +24,24 @@ clicked.addEventListener("click", (event) => {
 })
 
 treeBtn.addEventListener("click", (event) => {
+    // event.preventDefault(); 
+     if(toggle){
+         toggle = false;
+         makeDiv.textContent = "Hello";
+         makeDiv.style.color = "red";
+         main.appendChild(makeDiv);
+         console.log("active");
+ 
+     } else {
+         toggle = true;
+         main.removeChild(makeDiv);
+         console.log("inactive")
+     }
+ 
+     console.log("Clicked Successfull");
+ })
+
+ addTextBtn.addEventListener("click", (event) => {
     // event.preventDefault(); 
      if(toggle){
          toggle = false;
