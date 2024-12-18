10de5a506cf94c76abf5592cc8335f55e74fefb6
bernst4315 <bernst4315@gmail.com>
2024-12-18 15:16:59 -0700

    Button now adds/removes a div with text

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const clicked = document.getElementById("btn");
+const main = document.querySelector("#main")
 let toggle = true;
+let makeDiv = document.createElement("div");
 
 clicked.addEventListener("click", (event) => {
    // event.preventDefault(); 
     if(toggle){
         toggle = false;
+        makeDiv.textContent = "Hello";
+        makeDiv.style.color = "red";
+        main.appendChild(makeDiv);
         console.log("active");
+
     } else {
         toggle = true;
+        main.removeChild(makeDiv);
         console.log("inactive")
     }
+
     console.log("Clicked Successfull");
 })
 
