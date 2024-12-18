93b313be8b6af38d2676dc5c484793a339dc8eee
bernst4315 <bernst4315@gmail.com>
2024-12-18 14:35:22 -0700

    created a toggle button

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const clicked = document.getElementById("btn");
+let toggle = true;
 
-clicked.addEventListener("click", () => {
+clicked.addEventListener("click", (event) => {
+   // event.preventDefault(); 
+    if(toggle){
+        toggle = false;
+        console.log("active");
+    } else {
+        toggle = true;
+        console.log("inactive")
+    }
     console.log("Clicked Successfull");
 })
 
